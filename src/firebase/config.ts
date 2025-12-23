bc67841b95471aaedbce1412e66ef3670eab1090
Darko Haidar <darko.h.hussain@gmail.com>
2025-12-23 09:29:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1995603560-fe228",
+  "appId": "1:400653600210:web:177c58d071e2d764ac340b",
+  "apiKey": "AIzaSyAEOaydFqWeZfiUzV57KWvxBW2BH67ybpc",
+  "authDomain": "studio-1995603560-fe228.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "400653600210"
+};
