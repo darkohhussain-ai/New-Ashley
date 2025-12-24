d1656b2a508bcd66fc492bd53eec16a474273593
Darko Haidar <darko.h.hussain@gmail.com>
2025-12-24 19:53:25 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://i.ibb.c

--- next.config.ts
+++ next.config.ts
@@ -35,6 +35,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
   ...(process.env.NODE_ENV === 'development' && {
