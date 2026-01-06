9adcd67f6db0879f4f6e4c76e72769a1821fb2b9
Darko Haidar <darko.h.hussain@gmail.com>
2026-01-06 15:55:32 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,7 @@
 
 'use client';
 
-import dynamic from 'next/dynamic';
-import { Skeleton } from '@/components/ui/skeleton';
-
-const DashboardClient = dynamic(
-  () => import('@/components/dashboard/dashboard-client').then((mod) => mod.DashboardClient),
-  {
-    ssr: false,
-    loading: () => <DashboardSkeleton />,
-  }
-);
-
-function DashboardSkeleton() {
-    return (
-        <main className="container mx-auto p-4 md:p-8">
-            <div className="mb-8">
-                <Skeleton className="h-8 w-64 mb-2" />
-                <Skeleton className="h-4 w-80" />
-            </div>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {[...Array(7)].map((_, i) => (
-                    <Skeleton key={i} className="h-48 w-full" />
-                ))}
-            </div>
-        </main>
-    );
-}
-
+import { DashboardClient } from '@/components/dashboard/dashboard-client';
 
 export default function Home() {
   return <DashboardClient />;
