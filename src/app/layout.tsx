bc17a3d0ff4827b41180c8f264b7ad03a83d4454
Darko Haidar <darko.h.hussain@gmail.com>
2026-01-08 09:29:55 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${notoNaskhArabic.variable} font-sans antialiased`}>
+      <body className={`${notoNaskhArabic.variable} font-sans antialiased`} suppressHydrationWarning>
         {showSplash ? (
           <SplashScreen />
         ) : (
