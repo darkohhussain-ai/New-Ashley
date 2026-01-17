208095fc4af68bae19e41be2457834b810f52e19
Darko Haidar <darko.h.hussain@gmail.com>
2026-01-17 07:54:39 +0000

    i have this issue when i want depoly in > Build error occurred [Error: P

--- next.config.js
+++ next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-// This will be the name of your GitHub repository.
-// For example, if your repository URL is https://github.com/user/my-website,
-// then the repoName is "my-website".
-const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : '';
-
 const nextConfig = {
-  // Enable static export
-  output: 'export',
-
-  // Set the base path and asset prefix for GitHub Pages
-  basePath: repoName ? `/${repoName}` : '',
-  assetPrefix: repoName ? `/${repoName}/` : '',
-
   /* Other config options */
   typescript: {
     ignoreBuildErrors: true,
@@ -21,8 +9,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    // GitHub Pages static export doesn't support the default Next.js image optimization.
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
